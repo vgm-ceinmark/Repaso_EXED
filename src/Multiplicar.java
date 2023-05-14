5243a38791c6f1256690748ff5fd6e3fa8d83b57
vgm-ceinmark <vgarciam@ceinmark.net>
2023-05-14 14:04:26 +0200

    Primer commit tablas de multiplicar

---
+++ src/Multiplicar.java
@@ -0,0 +1,12 @@
+
+public class Multiplicar {
+
+	public static void main(String[] args) {
+		for(int i=1;i<11;i++) {
+			for(int j=0;i<11;i++) {
+				System.out.println(i+" x "+j+" = "+i*j);
+			}
+		}
+	}
+	
+}
